8e97de5a1e398ad92315e366d8e38731b27dfc42
toinomori-app <yuico3mama@gmail.com>
2026-07-09 13:46:56 +0900

    Update meishiki.js

--- meishiki.js
+++ meishiki.js
@@ -2,3 +2,32 @@
 // Ver0.1
 
 console.log("meishiki.js loaded");
+function createMeishiki(year, month, day, hour = null, minute = null) {
+
+  return {
+
+    year,
+    month,
+    day,
+    hour,
+    minute,
+
+    nen: null,
+    getsu: null,
+    nichi: null,
+    ji: null,
+
+    dayStem: null,
+    monthBranch: null,
+    hiddenStem: null,
+
+    tsuhen: null,
+    juniun: null,
+
+    residentNo: null,
+    meguriKana: null,
+    moonName: null
+
+  };
+
+}
